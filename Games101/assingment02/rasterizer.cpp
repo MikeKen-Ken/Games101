3ad7337039463fcddb21d4d1cd72f117f017018b
chenken <aken112981@163.com>
2021-09-17 17:22:21 +0800

    fix assignment bug

--- Games101/assingment02/rasterizer.cpp
+++ Games101/assingment02/rasterizer.cpp
@@ -96,7 +96,7 @@ void rst::rasterizer::draw(pos_buf_id pos_buffer, ind_buf_id ind_buffer, col_buf
         {
             vert.x() = 0.5 * width * (vert.x() + 1.0);
             vert.y() = 0.5 * height * (vert.y() + 1.0);
-            vert.z() = -vert.z() * f1 + f2;
+            vert.z() = vert.z() * f1 + f2;
         }
 
         for (int i = 0; i < 3; ++i)
@@ -140,7 +140,7 @@ void rst::rasterizer::rasterize_triangle(const Triangle &t)
     Vector3f colorWeight;
     float weight;
 
-    bool SSAA = true;
+    bool SSAA = false;
 
     // TODO :Find out the bounding box of current triangle.
     // TODO :iterate through the pixel and find if the current pixel is inside the triangle
@@ -194,9 +194,9 @@ void rst::rasterizer::rasterize_triangle(const Triangle &t)
                     w_reciprocal = 1.0 / (alpha / v[0].w() + beta / v[1].w() + gamma / v[2].w());
                     z_interpolated = alpha * v[0].z() / v[0].w() + beta * v[1].z() / v[1].w() + gamma * v[2].z() / v[2].w();
                     z_interpolated *= w_reciprocal;
-
+                    z_interpolated = -z_interpolated;
                     // TODO : set the current pixel (use the set_pixel function) to the color of the triangle (use getColor function) if it should be painted.
-                    //* z进行了反转，所以z越大距离视点越远
+                    //* deptp_buf 初始值为+inf，先转换z_value为正值，之后再做比较
                     if (z_interpolated < depth_buf[get_index(x, y)])
                     {
                         point << (float)x, (float)y, 1.0;
