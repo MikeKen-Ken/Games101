fd72d9b6e6d08cdf00a354c136c40986a2343484
chenken <aken112981@163.com>
2021-10-13 11:36:06 +0800

    直射光采样

--- Games101/assignment07/Scene.cpp
+++ Games101/assignment07/Scene.cpp
@@ -4,8 +4,8 @@
 
 #include "Scene.hpp"
 
-
-void Scene::buildBVH() {
+void Scene::buildBVH()
+{
     printf(" - Generating BVH...\n\n");
     this->bvh = new BVHAccel(objects, 1, BVHAccel::SplitMethod::NAIVE);
 }
@@ -18,17 +18,22 @@ Intersection Scene::intersect(const Ray &ray) const
 void Scene::sampleLight(Intersection &pos, float &pdf) const
 {
     float emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
+    for (uint32_t k = 0; k < objects.size(); ++k)
+    {
+        if (objects[k]->hasEmit())
+        {
             emit_area_sum += objects[k]->getArea();
         }
     }
     float p = get_random_float() * emit_area_sum;
     emit_area_sum = 0;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
-        if (objects[k]->hasEmit()){
+    for (uint32_t k = 0; k < objects.size(); ++k)
+    {
+        if (objects[k]->hasEmit())
+        {
             emit_area_sum += objects[k]->getArea();
-            if (p <= emit_area_sum){
+            if (p <= emit_area_sum)
+            {
                 objects[k]->Sample(pos, pdf);
                 break;
             }
@@ -37,22 +42,23 @@ void Scene::sampleLight(Intersection &pos, float &pdf) const
 }
 
 bool Scene::trace(
-        const Ray &ray,
-        const std::vector<Object*> &objects,
-        float &tNear, uint32_t &index, Object **hitObject)
+    const Ray &ray,
+    const std::vector<Object *> &objects,
+    float &tNear, uint32_t &index, Object **hitObject)
 {
     *hitObject = nullptr;
-    for (uint32_t k = 0; k < objects.size(); ++k) {
+    for (uint32_t k = 0; k < objects.size(); ++k)
+    {
         float tNearK = kInfinity;
         uint32_t indexK;
         Vector2f uvK;
-        if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear) {
+        if (objects[k]->intersect(ray, tNearK, indexK) && tNearK < tNear)
+        {
             *hitObject = objects[k];
             tNear = tNearK;
             index = indexK;
         }
     }
-
 
     return (*hitObject != nullptr);
 }
@@ -60,5 +66,19 @@ bool Scene::trace(
 // Implementation of Path Tracing
 Vector3f Scene::castRay(const Ray &ray, int depth) const
 {
-    // TO DO Implement Path Tracing Algorithm here
+    // TODO Implement Path Tracing Algorithm here
+    Intersection inter = intersect(ray);
+
+    //计算光源面积
+    float emit_area_sum = 0;
+    for (uint32_t k = 0; k < objects.size(); ++k)
+    {
+        if (objects[k]->hasEmit())
+        {
+            emit_area_sum += objects[k]->getArea();
+        }
+    }
+
+    float lightPdf = 1.0f / emit_area_sum;
+    sampleLight(inter, lightPdf);
 }
